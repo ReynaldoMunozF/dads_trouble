122907986807e031e110f5f4be6b9b969d99d8cc
Reynaldo <reynaldo.munozf21@gmail.com>
2024-03-06 12:46:49 +0100

    create endpoints users - families

--- src/routes/authUser.routes.ts
+++ src/routes/authUser.routes.ts
@@ -7,6 +7,7 @@ const router = express.Router();
 const authUserController = new AuthUserController();
 
 router.post("/register", authUserController.register);
-router.post("/login", authUserController.login);
+router.post("/login", authUserController.login); 
+ // --------------------------
 
 export default router;
